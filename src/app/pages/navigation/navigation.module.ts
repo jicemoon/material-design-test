032c3538b65dbdab3f1bfd2868d27ba837fe3b8a
jicemoon <jicemoon@outlook.com>
2018-06-09 15:25:34 +0800

    add navigation/menu test

--- src/app/pages/navigation/navigation.module.ts
+++ src/app/pages/navigation/navigation.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatMenuModule } from '@angular/material';
+
 import { NavigationRoutingModule } from './navigation-routing.module';
 import { NavigationComponent } from './navigation.component';
 import { MenuComponent } from './menu/menu.component';
@@ -8,6 +10,7 @@ import { MenuComponent } from './menu/menu.component';
 @NgModule({
   imports: [
     CommonModule,
+    MatMenuModule,
     NavigationRoutingModule
   ],
   declarations: [NavigationComponent, MenuComponent]
