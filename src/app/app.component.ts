b971af8e0f1ee464986dea60ba06a99b61644137
reese <reese@ying31.com>
2018-12-04 17:40:12 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,8 @@ import { getFadeTransAnimaiton } from './shared/animations';
 export class AppComponent {
   opened = false;
 
-  navClickHandle(evt) {
-    const nodeName = ((<MouseEvent>evt).target as HTMLElement).nodeName.toLowerCase();
+  navClickHandle(evt: MouseEvent) {
+    const nodeName = (evt.target as HTMLElement).nodeName.toLowerCase();
     if (nodeName === 'span' || nodeName === 'a') {
       this.opened = false;
     }
